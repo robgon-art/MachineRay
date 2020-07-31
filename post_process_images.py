5776e7a41f9f183767ecf48f3a3453799e0347c2
Rob Gonsalves <robgonsalves@gmail.com>
2020-07-31 13:17:27 -0400

    Update post_process_images.py

--- post_process_images.py
+++ post_process_images.py
@@ -2,19 +2,30 @@ import numpy as np
 from PIL import Image
 from ISR.models import RDN, RRDN
 
+# Import the image
 img = Image.open('input.png')
+
+# Load the GAN model that will perform a 4x resize
 model = RRDN(weights='gans')
 
+# Convert to numpy image
 npimg = np.array(img)
 row,col,ch= npimg.shape
+
+# Add some npise
 mean = 0
 var = 0.1
 sigma = var**0.5
 gauss = np.random.normal(mean,sigma,(row,col,ch))*24
+
+# Reshape and clip the pixel values
 gauss = gauss.reshape(row,col,ch)
 noisy = np.clip(npimg + gauss, 0, 255).astype('uint8')
 noisy_image = Image.fromarray(noisy)
-big_np = model.predict(np.array(noisy_image))
-big_img = Image.fromarray(big_np)
 
+# Do the resize
+big_np = model.predict(np.array(noisy_image))
+
+# Save the image
+big_img = Image.fromarray(big_np)
 big_img.save("big.jpg")
